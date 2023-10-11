ab02667ab9ce9ef1f115fb9ceb2b8669c743b527
tomdcht <tom.dechanet@gmail.com>
2023-10-11 19:04:47 +0200

    Change implements DAO to AbstractList

--- tp3/src/main/java/fr/univlyon1/m1if/m1if03/daos/TodoDao.java
+++ tp3/src/main/java/fr/univlyon1/m1if/m1if03/daos/TodoDao.java
@@ -4,44 +4,11 @@ import javax.naming.InvalidNameException;
 import javax.naming.NameAlreadyBoundException;
 import javax.naming.NameNotFoundException;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Collection;
 
-public class TodoDao implements Dao {
+public class TodoDao extends AbstractListDao {
 
 
-    @Override
-    public Serializable add(Object element) throws NameAlreadyBoundException {
-        return null;
-    }
-
-    @Override
-    public void delete(Object element) throws NameNotFoundException {
-
-    }
-
-    @Override
-    public void deleteById(Serializable id) throws NameNotFoundException, InvalidNameException {
-
-    }
-
-    @Override
-    public void update(Serializable id, Object element) throws InvalidNameException {
-
-    }
-
-    @Override
-    public Serializable getId(Object element) throws NameNotFoundException {
-        return null;
-    }
-
-    @Override
-    public Object findOne(Serializable id) throws NameNotFoundException, InvalidNameException {
-        return null;
-    }
-
-    @Override
-    public Collection findAll() {
-        return null;
-    }
 }
 
