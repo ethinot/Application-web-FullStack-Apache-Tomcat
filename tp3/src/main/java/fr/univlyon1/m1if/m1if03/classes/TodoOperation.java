aad90d70be7f73b268a402b4254bf3c03cfef719
Edouard Thinot <edouard.thinot@etu.univ-lyon1.fr>
2023-10-16 23:14:47 +0200

    Correct TodoOperation interface by changing not known method

--- tp3/src/main/java/fr/univlyon1/m1if/m1if03/classes/TodoOperation.java
+++ tp3/src/main/java/fr/univlyon1/m1if/m1if03/classes/TodoOperation.java
@@ -1,12 +1,12 @@
 package fr.univlyon1.m1if.m1if03.classes;
 
+import fr.univlyon1.m1if.m1if03.daos.AbstractListDao;
 import fr.univlyon1.m1if.m1if03.daos.Dao;
 import fr.univlyon1.m1if.m1if03.exceptions.MissingParameterException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
-import java.util.List;
 import java.util.Objects;
 
 /**
@@ -15,13 +15,22 @@ import java.util.Objects;
  */
 public interface TodoOperation {
 
+    /**
+     * Method used for adding a new Todo objet to the todos DAO.
+     *
+     * @param request the HTTP/HTTPS request
+     * @param response the HTTP/HTTPS response we want to build
+     * @throws IOException throw exception if IN/OUT outputs are incorrect
+     */
     static void add(HttpServletRequest request, HttpServletResponse response) throws IOException {
         try {
-            List<Todo> todos = (List<Todo>) request.getServletContext().getAttribute("todos");
+            AbstractListDao<Todo> todos = (AbstractListDao<Todo>) request.getServletContext().getAttribute("todos");
             if (request.getParameter("title") == null || request.getParameter("login") == null) {
                 throw new MissingParameterException("Paramètres du Todo insuffisamment spécifiés.");
             }
             todos.add(new Todo(request.getParameter("title"), request.getParameter("login")));
+            request.setAttribute("todos", todos);
+            request.getRequestDispatcher("todolist.jsp").include(request, response);
         } catch (NumberFormatException e) {
             response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Format de l'index du User incorrect.");
         } catch (Exception e) {
@@ -30,18 +39,20 @@ public interface TodoOperation {
     }
 
     /**
-     * @param request 
-     * @param response
-     * @throws IOException
+     * Methode to update a specefic Todo from the DAO todos.
+     *
+     * @param request the HTTP/HTTPS request
+     * @param response the HTTP/HTTPS response we want to build
+     * @throws IOException throw exception if IN/OUT outputs are incorrect
      */
     static void update(HttpServletRequest request, HttpServletResponse response) throws IOException {
         try {
-            List<Todo> todos = (List<Todo>) request.getServletContext().getAttribute("todos");
+            AbstractListDao<Todo> todos = (AbstractListDao<Todo>) request.getServletContext().getAttribute("todos");
             int index = Integer.parseInt(request.getParameter("index"));
             if (index < 0 || index >= todos.size()) {
                 throw new StringIndexOutOfBoundsException("Pas de todo avec l'index " + index + ".");
             }
-            Todo todo = todos.get(index);
+            Todo todo = todos.findOne(index);
             if (request.getParameter("toggle") != null && !request.getParameter("toggle").isEmpty()) {
                 todo.setCompleted(Objects.equals(request.getParameter("toggle"), "Done!"));
             } else {
