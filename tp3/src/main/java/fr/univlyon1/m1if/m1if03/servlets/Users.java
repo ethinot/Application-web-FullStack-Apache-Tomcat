57c5b84f381009cce8e5f21163e002c767ed391d
Edouard Thinot <edouard.thinot@etu.univ-lyon1.fr>
2023-10-16 20:36:17 +0200

    First version of Users controler that implement user connection/disconnection

--- tp3/src/main/java/fr/univlyon1/m1if/m1if03/servlets/Users.java
+++ tp3/src/main/java/fr/univlyon1/m1if/m1if03/servlets/Users.java
@@ -1,18 +1,13 @@
 package fr.univlyon1.m1if.m1if03.servlets;
 
-import fr.univlyon1.m1if.m1if03.classes.User;
-import fr.univlyon1.m1if.m1if03.daos.Dao;
+import fr.univlyon1.m1if.m1if03.classes.Connect;
 import jakarta.servlet.ServletConfig;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
 
-import javax.naming.InvalidNameException;
-import javax.naming.NameAlreadyBoundException;
-import javax.naming.NameNotFoundException;
 import java.io.IOException;
 
 /**
@@ -20,8 +15,8 @@ import java.io.IOException;
  *
  *
  */
-@WebServlet(name = "UserMap", value = "/user")
-public class Users extends HttpServlet {
+@WebServlet(name = "Users", value = "/users")
+public class Users extends HttpServlet implements Connect {
     @Override
     public void init(ServletConfig config) throws ServletException {
         super.init(config);
@@ -30,25 +25,11 @@ public class Users extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         try {
-            Dao<User> users = (Dao<User>) this.getServletContext().getAttribute("users");
             switch (request.getParameter("operation")) {
                 case "add" -> {
-                    // Gestion de la création de l'utilisateur
-                    User newUser = new User(request.getParameter("login"), request.getParameter("name"));
-                    try {
-                        users.add(new User(request.getParameter("login"), request.getParameter("name")));
-                    } catch (NameAlreadyBoundException e) {
-                        response.sendError(HttpServletResponse.SC_BAD_REQUEST, "The key used for add the new user is already used.");
-                    }
-                    // Gestion de la session utilisateur
-                    String login = request.getParameter("login");
-                    HttpSession session = request.getSession(true);
-                    session.setAttribute("login", login);
-                    response.sendRedirect("interface.jsp");
+                    Connect.connect(request, response);
                 }
-                case "del" -> {
-                    deleteUser(request, response);
-                }
+
                 default -> throw new UnsupportedOperationException("Opération à réaliser non prise en charge.");
             }
         } catch (NumberFormatException e) {
@@ -62,19 +43,6 @@ public class Users extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-
-    }
-
-    protected void deleteUser(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        HttpSession session = request.getSession(false);
-        String login = null;
-        try {
-            login = (String) session.getAttribute("login");
-            session.invalidate();
-            ((Dao<User>) this.getServletContext().getAttribute("users")).deleteById(login);
-            response.sendRedirect("index.html");
-        } catch (NameNotFoundException | InvalidNameException e) {
-            response.sendError(HttpServletResponse.SC_NOT_FOUND, "Le login de l'utilisateur courant est erroné : " + login + ".");
-        }
+        Connect.disconnect(request, response);
     }
 }
