376ef671114f2291f162572d87764c7221cd0efc
Edouard Thinot <edouard.thinot@etu.univ-lyon1.fr>
2023-10-18 17:44:00 +0200

    Begin implement of TodoResourceController and TodoResource (POST methods done)

--- tp4/src/main/java/fr/univlyon1/m1if/m1if03/controllers/TodoResourceController.java
+++ tp4/src/main/java/fr/univlyon1/m1if/m1if03/controllers/TodoResourceController.java
@@ -1,4 +1,219 @@
 package fr.univlyon1.m1if.m1if03.controllers;
 
-public class TodoResourceController {
+import fr.univlyon1.m1if.m1if03.controllers.resources.TodoResource;
+import fr.univlyon1.m1if.m1if03.dao.TodoDao;
+import fr.univlyon1.m1if.m1if03.dao.UserDao;
+import fr.univlyon1.m1if.m1if03.dto.todo.TodoDtoMapper;
+import fr.univlyon1.m1if.m1if03.dto.user.UserDtoMapper;
+import fr.univlyon1.m1if.m1if03.dto.user.UserResponseDto;
+import fr.univlyon1.m1if.m1if03.exceptions.ForbiddenLoginException;
+import fr.univlyon1.m1if.m1if03.model.Todo;
+import fr.univlyon1.m1if.m1if03.model.User;
+import fr.univlyon1.m1if.m1if03.utils.UrlUtils;
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.constraints.NotNull;
+
+import javax.naming.InvalidNameException;
+import javax.naming.NameAlreadyBoundException;
+import javax.naming.NameNotFoundException;
+import java.io.IOException;
+import java.util.Collection;
+
+/**
+ * Contrôleur de ressources "todos".
+ * Gère les CU liés aux opérations CRUD sur la collection de todos :
+ *     Création / modification / suppression d'un toto : POST, PUT, DELETE
+ *     Récupération de la liste de todos / de todo en lien avec un utilisateur / d'un todo en particulier : GET
+ * Cette servlet fait appel à une nested class UserResource</code> qui se charge des appels au DAO.
+ * Les opérations métier spécifiques (login &amp; logout) sont réalisées par la servlet <a href="UserBusinessController.html"><code>UserBusinessController</code></a>.
+ *
+ * @author Lionel Médini
+ */
+@WebServlet(name = "UserResourceController", urlPatterns = {"/todos", "/todos/*"})
+public class TodoResourceController extends HttpServlet {
+    private TodoDtoMapper todoMapper;
+    private TodoResource todoResource;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        TodoDao todoDao = (TodoDao) config.getServletContext().getAttribute("todoDao");
+        todoMapper = new TodoDtoMapper(config.getServletContext());
+        todoResource = new TodoResource(todoDao);
+    }
+    //</editor-fold>
+
+    //<editor-fold desc="Méthodes de service">
+    /**
+     * Réalise la création d'un utilisateur.
+     * Renvoie un code 201 (Created) en cas de création d'un utilisateur, ou une erreur HTTP appropriée sinon.
+     *
+     * @param request  Une requête de création, contenant un body avec un login, un password et un nom
+     * @param response Une réponse vide, avec uniquement un code de réponse et éventuellement un header <code>Location</code>
+     * @throws IOException Voir doc...
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setHeader("X-test", "doPost");
+
+        String[] url = UrlUtils.getUrlParts(request);
+
+        if (url.length == 1) {// Création d'un utilisateur
+            // TODO Parsing des paramètres "old school" ; sera amélioré dans la partie négociation de contenus...
+            String login = request.getParameter("login");
+            String password = request.getParameter("password");
+            String name = request.getParameter("name");
+            try {
+                userResource.create(login, password, name);
+                response.setHeader("Location", "users/" + login);
+                response.setStatus(HttpServletResponse.SC_CREATED);
+            } catch (IllegalArgumentException | ForbiddenLoginException ex) {
+                response.sendError(HttpServletResponse.SC_BAD_REQUEST, ex.getMessage());
+            } catch (NameAlreadyBoundException e) {
+                response.sendError(HttpServletResponse.SC_CONFLICT, "Le login " + login + " n'est plus disponible.");
+            }
+        } else {
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+        }
+    }
+
+    /**
+     * Traite les requêtes GET.
+     * Renvoie une représentation de la ressource demandée.
+     * <ul>
+     *     <li>Soit la liste de tous les utilisateurs</li>
+     *     <li>Soit un utilisateur</li>
+     *     <li>soit une propriété d'un utilisateur</li>
+     *     <li>soit une redirection vers une sous-propriété</li>
+     * </ul>
+     * Renvoie un code de réponse 200 (OK) en cas de représentation, 302 (Found) en cas de redirection, sinon une erreur HTTP appropriée.
+     *
+     * @param request  Une requête vide
+     * @param response Une réponse contenant :
+     *                 <ul>
+     *                     <li>la liste des liens vers les instances de <code>User</code> existantes</li>
+     *                     <li>les propriétés d'un utilisateur</li>
+     *                     <li>une propriété donnée d'un utilisateur donné</li>
+     *                 </ul>
+     * @throws ServletException Voir doc...
+     * @throws IOException      Voir doc...
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setHeader("X-test", "doGet");
+        String[] url = UrlUtils.getUrlParts(request);
+        if (url.length == 1) { // Renvoie la liste de tous les utilisateurs
+            request.setAttribute("users", todoResource.readAll());
+            // Transfère la gestion de l'interface à une JSP
+            request.getRequestDispatcher("/WEB-INF/components/users.jsp").include(request, response);
+            return;
+        }
+        try {
+            Todo todo = todoResource.readOne(url[1]);
+            UserResponseDto userDto = todoMapper.toDto(todo);
+            switch (url.length) {
+
+                default -> { // Redirige vers l'URL qui devrait correspondre à la sous-propriété demandée (qu'elle existe ou pas ne concerne pas ce contrôleur)
+                    if (url[2].equals("assignedTodos")) {
+                        // Construction de la fin de l'URL vers laquelle rediriger
+                        String urlEnd = UrlUtils.getUrlEnd(request, 3);
+                        response.sendRedirect("todos" + urlEnd);
+                    } else {
+                        response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+                    }
+                }
+            }
+        } catch (IllegalArgumentException ex) {
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST, ex.getMessage());
+        } catch (NameNotFoundException e) {
+            response.sendError(HttpServletResponse.SC_NOT_FOUND, "L'utilisateur " + url[1] + " n'existe pas.");
+        } catch (InvalidNameException ignored) {
+            // Ne devrait pas arriver car les paramètres sont déjà des Strings
+        }
+    }
+
+    /**
+     * Réalise la modification d'un utilisateur.
+     * En fonction du login passé dans l'URL :
+     * <ul>
+     *     <li>création de l'utilisateur s'il n'existe pas</li>
+     *     <li>Mise à jour sinon</li>
+     * </ul>
+     * Renvoie un code de statut 204 (No Content) en cas de succès ou une erreur HTTP appropriée sinon.
+     *
+     * @param request  Une requête dont l'URL est de la forme <code>/users/{login}</code>, et contenant :
+     *                 <ul>
+     *                     <li>un password (obligatoire en cas de création)</li>
+     *                     <li>un nom (obligatoire en cas de création)</li>
+     *                 </ul>
+     * @param response Une réponse vide (si succès)
+     * @throws IOException Voir doc...
+     */
+    @Override
+    protected void doPut(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String[] url = UrlUtils.getUrlParts(request);
+        String login = url[1];
+        // TODO Parsing des paramètres "old school" ; sera amélioré dans la partie négociation de contenus...
+        String password = request.getParameter("password");
+        String name = request.getParameter("name");
+
+        if (url.length == 2) {
+            try {
+                userResource.update(login, password, name);
+                response.setStatus(HttpServletResponse.SC_NO_CONTENT);
+            } catch (IllegalArgumentException ex) {
+                response.sendError(HttpServletResponse.SC_BAD_REQUEST, ex.getMessage());
+            } catch (NameNotFoundException e) {
+                try {
+                    userResource.create(login, password, name);
+                    response.setHeader("Location", "users/" + login);
+                    response.setStatus(HttpServletResponse.SC_CREATED);
+                } catch (NameAlreadyBoundException ignored) {
+                } catch (IllegalArgumentException | ForbiddenLoginException ex) {
+                    response.sendError(HttpServletResponse.SC_BAD_REQUEST, ex.getMessage());
+                }
+            } catch (InvalidNameException ignored) {
+                // Ne devrait pas arriver car les paramètres sont déjà des Strings
+            }
+        } else {
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+        }
+    }
+
+    /**
+     * Réalise l'aiguillage des requêtes DELETE.<br>
+     * En clair : appelle simplement l'opération de suppression de l'utilisateur.<br>
+     * Renvoie un code 204 (No Content) si succès ou une erreur HTTP appropriée sinon.
+     *
+     * @param request  Une requête dont l'URL est de la forme <code>/users/{login}</code>
+     * @param response Une réponse vide (si succès)
+     * @throws IOException Voir doc...
+     */
+    @Override
+    protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String[] url = UrlUtils.getUrlParts(request);
+        String login = url[1];
+        if (url.length == 2) {
+            try {
+                userResource.delete(login);
+                response.setStatus(HttpServletResponse.SC_NO_CONTENT);
+            } catch (IllegalArgumentException ex) {
+                response.sendError(HttpServletResponse.SC_BAD_REQUEST, ex.getMessage());
+            } catch (NameNotFoundException e) {
+                response.sendError(HttpServletResponse.SC_NOT_FOUND, "L'utilisateur " + login + " n'existe pas.");
+            } catch (InvalidNameException ignored) {
+                // Ne devrait pas arriver car les paramètres sont déjà des Strings
+            }
+        } else {
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+        }
+    }
+    //</editor-fold>
+
+    //<editor-fold desc="Nested class qui interagit avec le DAO">
 }
--- tp4/src/main/java/fr/univlyon1/m1if/m1if03/controllers/resources/TodoResource.java
+++ tp4/src/main/java/fr/univlyon1/m1if/m1if03/controllers/resources/TodoResource.java
@@ -1,4 +1,49 @@
 package fr.univlyon1.m1if.m1if03.controllers.resources;
 
+import fr.univlyon1.m1if.m1if03.dao.TodoDao;
+import fr.univlyon1.m1if.m1if03.dao.UserDao;
+import fr.univlyon1.m1if.m1if03.exceptions.ForbiddenLoginException;
+import fr.univlyon1.m1if.m1if03.model.Todo;
+import fr.univlyon1.m1if.m1if03.model.User;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
+
+import javax.naming.InvalidNameException;
+import javax.naming.NameAlreadyBoundException;
+import javax.naming.NameNotFoundException;
+import java.util.Collection;
+
 public class TodoResource {
+    private final TodoDao todoDao;
+
+    /**
+     * Constructeur avec une injection du DAO nécessaire aux opérations.
+     * @param totoDao le DAO de todo provenant du contexte applicatif
+     */
+    public TodoResource(TodoDao totoDao) {
+        this.todoDao = totoDao;
+    }
+
+    /**
+     * Crée un toto et le place dans le DAO.
+     *
+     * @param titre Titre de la todo créée
+     * @param creatorLogin Login de l'utilisateur qui crée la todo
+     * @throws IllegalArgumentException Si le creatorLogin ou titre est null ou vide
+     */
+    public void create(@NotNull String titre, @NotNull String creatorLogin) throws IllegalArgumentException {
+        if (creatorLogin == null || creatorLogin.isEmpty()) {
+            throw new IllegalArgumentException("Le login du créateur ne doit pas être null ou vide.");
+        }
+        if (titre == null) {
+            throw new IllegalArgumentException("Le titre ne doit pas être null.");
+        }
+        todoDao.add(new Todo(titre, creatorLogin));
+    }
+
+    public void modifyStatus(@Positive int todoHash) {
+        boolean actuelStatus = todoDao.findByHash(todoHash).isCompleted();
+        todoDao.findByHash(todoHash).setCompleted(!actuelStatus);
+    }
 }
