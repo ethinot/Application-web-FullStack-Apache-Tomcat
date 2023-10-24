1adee1e5a601b6571783fcba4b77eefcd81c46a6
Edouard Thinot <edouard.thinot@etu.univ-lyon1.fr>
2023-10-24 22:02:14 +0200

    Implement the railroad switch for different types of request that concerne todo

--- tp4/src/main/java/fr/univlyon1/m1if/m1if03/controllers/TodoResourceController.java
+++ tp4/src/main/java/fr/univlyon1/m1if/m1if03/controllers/TodoResourceController.java
@@ -3,6 +3,9 @@ package fr.univlyon1.m1if.m1if03.controllers;
 import fr.univlyon1.m1if.m1if03.controllers.resources.TodoResource;
 import fr.univlyon1.m1if.m1if03.dao.TodoDao;
 import fr.univlyon1.m1if.m1if03.dto.todo.TodoDtoMapper;
+import fr.univlyon1.m1if.m1if03.dto.todo.TodoResponseDto;
+import fr.univlyon1.m1if.m1if03.model.Todo;
+import fr.univlyon1.m1if.m1if03.utils.UrlUtils;
 import jakarta.servlet.ServletConfig;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
@@ -10,6 +13,7 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import javax.naming.NameNotFoundException;
 import java.io.IOException;
 
 /**
@@ -38,64 +42,108 @@ public class TodoResourceController extends HttpServlet {
 
     //<editor-fold desc="Méthodes de service">
     /**
-     * Réalise la création d'un utilisateur.
+     * Réalise la création d'un todo.
      * Renvoie un code 201 (Created) en cas de création d'un utilisateur, ou une erreur HTTP appropriée sinon.
      *
      * @param request  Une requête de création, contenant un body avec un login, un password et un nom
      * @param response Une réponse vide, avec uniquement un code de réponse et éventuellement un header <code>Location</code>
-     * @throws IOException Voir doc...
+     * @throws IOException Input/output exception
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-
+        response.setHeader("X-test", "doPost");
+        String[] url = UrlUtils.getUrlParts(request);
+        if (url.length == 1) {
+            // Création d'un todo
+            String title = request.getParameter("titre");
+            String creator = request.getParameter("assignee");
+        }
     }
 
     /**
      * Traite les requêtes GET.
      * Renvoie une représentation de la ressource demandée.
-     * <ul>
-     *     <li>Soit la liste de tous les utilisateurs</li>
-     *     <li>Soit un utilisateur</li>
-     *     <li>soit une propriété d'un utilisateur</li>
-     *     <li>soit une redirection vers une sous-propriété</li>
-     * </ul>
-     * Renvoie un code de réponse 200 (OK) en cas de représentation, 302 (Found) en cas de redirection, sinon une erreur HTTP appropriée.
-     *
-     * @param request  Une requête vide
-     * @param response Une réponse contenant :
-     *                 <ul>
-     *                     <li>la liste des liens vers les instances de <code>User</code> existantes</li>
-     *                     <li>les propriétés d'un utilisateur</li>
-     *                     <li>une propriété donnée d'un utilisateur donné</li>
-     *                 </ul>
-     * @throws ServletException Voir doc...
-     * @throws IOException      Voir doc...
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        response.setHeader("X-test", "doGet");
+        String[] url = UrlUtils.getUrlParts(request);
+        if (url.length == 1) {
+            // Renvoie la collections de todos
+            request.setAttribute("todos", todoResource.readAll());
+        }
+        try {
+            Todo todo = todoResource.readOne(Integer.parseInt(url[1]));
+            TodoResponseDto todoDto = todoMapper.toDto(todo);
+            switch (url.length) {
+                case 2 -> {
+                    // Renvoie un DTO de todo
+                    request.setAttribute("totoDto", todoDto);
+                    request.getRequestDispatcher("/WEB-INF/components/user.jsp").include(request, response);
+                }
+                case 3 -> { // Renvoie une propriété d'un todo
+                    switch (url[2]) {
+                        case "title" -> {
+                            // Renvoie le titre d'un todo
+                        }
+                        case "assignee" -> {
+                            // Renvoie l'utilisateur auquel le todo est assigné
+                        }
+                        default -> response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+                    }
+                }
+                default -> { // Redirige vers l'URL qui devrait correspondre à la sous-propriété demandée (qu'elle existe ou pas ne concerne pas ce contrôleur)
+                    if (url[2].equals("assignee")) {
+                        // Construction de la fin de l'URL vers laquelle rediriger la sous-propriété
+                        String urlEnd = UrlUtils.getUrlEnd(request, 3);
+                        response.sendRedirect("users" + urlEnd);
+                    } else {
+                        response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+                    }
+                }
+            }
+        } catch (IllegalArgumentException ex) {
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST, ex.getMessage());
+        } /*catch (NameNotFoundException e) {
+            response.sendError(HttpServletResponse.SC_NOT_FOUND, "Le todo " + url[1] + " n'existe pas.");
+        }*/
     }
 
     /**
-     * Réalise la modification d'un utilisateur.
-     * En fonction du login passé dans l'URL :
-     * <ul>
-     *     <li>création de l'utilisateur s'il n'existe pas</li>
-     *     <li>Mise à jour sinon</li>
-     * </ul>
+     * Réalise la modification d'un todo.
+     * En fonction du l'id d'un todo (hash) passé dans l'URL :
+     *      - title
+     *      - assignee
      * Renvoie un code de statut 204 (No Content) en cas de succès ou une erreur HTTP appropriée sinon.
      *
-     * @param request  Une requête dont l'URL est de la forme <code>/users/{login}</code>, et contenant :
-     *                 <ul>
-     *                     <li>un password (obligatoire en cas de création)</li>
-     *                     <li>un nom (obligatoire en cas de création)</li>
-     *                 </ul>
+     * @param request  Une requête dont l'URL est de la forme /todos/{todoId}, et contenant :
      * @param response Une réponse vide (si succès)
      * @throws IOException Voir doc...
      */
     @Override
     protected void doPut(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String[] url = UrlUtils.getUrlParts(request);
+        int todoHash = Integer.parseInt(url[1]);
+        String title = request.getParameter("title");
+        String assignee = request.getParameter("assignee");
 
+        if (url.length == 2) {
+            try {
+                todoResource.update(todoHash, title, assignee);
+                response.setStatus(HttpServletResponse.SC_NO_CONTENT);
+            } catch (IllegalArgumentException ex1) {
+                response.sendError(HttpServletResponse.SC_BAD_REQUEST, ex1.getMessage());
+                try {
+                    todoResource.create(title, assignee);
+                    response.setHeader("Location", "todos/" + todoHash);
+                    response.setStatus(HttpServletResponse.SC_CREATED);
+                } catch (IllegalArgumentException ex2) {
+                    response.sendError(HttpServletResponse.SC_BAD_REQUEST, ex2.getMessage());
+                }
+            }
+        } else {
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+        }
     }
 
     /**
@@ -109,8 +157,19 @@ public class TodoResourceController extends HttpServlet {
      */
     @Override
     protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws IOException {
-
+        String[] url = UrlUtils.getUrlParts(request);
+        int todoId = Integer.parseInt(url[1]);
+        if (url.length == 2) {
+            try {
+                todoResource.delete(todoId);
+                response.setStatus(HttpServletResponse.SC_NO_CONTENT);
+            } catch (IllegalArgumentException ex) {
+                response.sendError(HttpServletResponse.SC_BAD_REQUEST, ex.getMessage());
+            } catch (NameNotFoundException e) {
+                response.sendError(HttpServletResponse.SC_NOT_FOUND, "Le todo avec l'id : " + todoId + " n'existe pas.");
+            }
+        } else {
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+        }
     }
-
-    //<editor-fold desc="Nested class qui interagit avec le DAO">
 }
