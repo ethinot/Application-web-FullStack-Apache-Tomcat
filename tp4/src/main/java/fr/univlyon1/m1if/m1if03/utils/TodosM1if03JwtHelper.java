d075dd0239122a6484416b607aaba18a635a5bb8
Edouard Thinot <edouard.thinot@etu.univ-lyon1.fr>
2023-11-12 20:44:56 +0100

    Add getAssigned function for getting the user assigned todo list by claiming it with is token

--- tp4/src/main/java/fr/univlyon1/m1if/m1if03/utils/TodosM1if03JwtHelper.java
+++ tp4/src/main/java/fr/univlyon1/m1if/m1if03/utils/TodosM1if03JwtHelper.java
@@ -4,11 +4,13 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
 import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.exceptions.TokenExpiredException;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.auth0.jwt.interfaces.JWTVerifier;
 
 import java.util.List;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.constraints.NotNull;
 
 import java.util.Date;
@@ -67,6 +69,27 @@ public final class TodosM1if03JwtHelper {
         }
     }
 
+
+    /**
+     * Function that claiming the user ids list.
+     *
+     * @param token the actuel user token
+     * @param login the login of the user (form the token payload) only for supplementary tests
+     * @return the ids list of todos affect to the user
+     */
+    public static List<Integer> getAssigned(String token, String login) {
+        try {
+            DecodedJWT jwt = JWT.decode(token);
+            if (login.equals(jwt.getSubject())) {
+                return jwt.getClaim("assignedTo").asList(Integer.class);
+            } else {
+                return null;
+            }
+        } catch (JWTVerificationException e) {
+            return null;
+        }
+    }
+    
     /**
      * Crée un token avec les caractéristiques de l'utilisateur.
      *
